089d92e4d676730ae1ad733b42a76aaeafcd69f7
Cole Howard <uglyboxer@gmail.com>
2018-04-22 08:57:16 -0700

    Add pass button

--- src/zetgo/display.py
+++ src/zetgo/display.py
@@ -11,6 +11,8 @@
 import pygame
 from game import Game
  
+BOARD_SIZE = 5
+game = Game(BOARD_SIZE)
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -27,11 +29,11 @@ MARGIN = 5
 # Create a 2 dimensional array. A two dimensional
 # array is simply a list of lists.
 grid = []
-for row in range(5):
+for row in range(BOARD_SIZE):
     # Add an empty array that will hold each cell
     # in this row
     grid.append([])
-    for column in range(5):
+    for column in range(BOARD_SIZE):
         grid[row].append(0)  # Append a cell
  
 # Set row 1, cell 5 to one. (Remember rows and
@@ -55,37 +57,43 @@ clock = pygame.time.Clock()
  
 # -------- Main Program Loop -----------
 
-game = Game(5)
-
 
 while not done and not game.passes[1]:
-    for event in pygame.event.get():  # User did something
+    # event = pygame.event.wait()
+
+    for event in pygame.event.get():
         print(event)
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
-        elif event.type == pygame.MOUSEMOTION:
-            pass
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONUP:
+        #     pos = pygame.mouse.get_pos()
+        #     continue
+        
+        # elif event.type == pygame.MOUSEMOTION:
             # User clicks the mouse. Get the position
             pos = pygame.mouse.get_pos()
             # Change the x/y screen coordinates to grid coordinates
             column = pos[0] // (WIDTH + MARGIN)
             row = pos[1] // (HEIGHT + MARGIN)
+            if column == BOARD_SIZE and row == BOARD_SIZE:
+                move = 'p'
+                rv = game.move(move)
 
-            rv = game.move('{}, {}'.format(row, column))
+            else:
+                move = '{}, {}'.format(row, column)
+                rv = game.move(move)
+                grid[row][column] = game.board.positions[row][column].player
             # Set that location to one
             if rv['complete']:
                 game.score()
-
-            grid[row][column] = game.board.positions[row][column].player
             print("Click ", pos, "Grid coordinates: ", row, column)
 
     # Set the screen background
     screen.fill(BLACK)
  
     # Draw the grid
-    for row in range(5):
-        for column in range(5):
+    for row in range(BOARD_SIZE):
+        for column in range(BOARD_SIZE):
             color = WHITE
             if game.board.positions[row][column].player == 1:
                 color = GREEN
@@ -97,12 +105,17 @@ while not done and not game.passes[1]:
                               (MARGIN + HEIGHT) * row + MARGIN,
                               WIDTH,
                               HEIGHT])
- 
+    pygame.draw.rect(screen, WHITE,
+        [(MARGIN + WIDTH) * BOARD_SIZE + MARGIN,
+         (MARGIN + HEIGHT) * BOARD_SIZE + MARGIN,
+         WIDTH,
+         HEIGHT]) 
     # Limit to 60 frames per second
     clock.tick(4)
     # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
+    pygame.display.update()
  
 # Be IDLE friendly. If you forget this line, the program will 'hang'
 # on exit.
+print(game.score())
 pygame.quit()
